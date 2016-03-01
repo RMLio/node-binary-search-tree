d6f1022e6eb5ff78b2580acd986495a55467dce1
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2016-03-01 15:35:39 +0100

    separate tree for extra array and no extra array for bloom filter

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 module.exports.BinarySearchTree = require('./lib/bst');
 module.exports.BinarySearchTreeSortedArray = require('./lib/bst-sorted-array');
+module.exports.BinarySearchTreeBloomFilterExtraArray = require('./lib/bst-bloomfilter-extra-array');
 module.exports.BinarySearchTreeBloomFilter = require('./lib/bst-bloomfilter');
 module.exports.AVLTree = require('./lib/avltree');
