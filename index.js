c2c85e622318be6c214693c955466258bd07d499
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2016-03-01 12:16:00 +0100

    added bst with sorted array

--- index.js
+++ index.js
@@ -1,2 +1,3 @@
 module.exports.BinarySearchTree = require('./lib/bst');
+module.exports.BinarySearchTreeSortedArray = require('./lib/bst-sorted-array');
 module.exports.AVLTree = require('./lib/avltree');
