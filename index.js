e5e001974344688d80e28d1d905bd703ab0de1d2
Pieter Heyvaert <pheyvaer.heyvaert@ugent.be>
2016-03-01 13:45:09 +0100

    bloom filter version added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
 module.exports.BinarySearchTree = require('./lib/bst');
 module.exports.BinarySearchTreeSortedArray = require('./lib/bst-sorted-array');
+module.exports.BinarySearchTreeBloomFilter = require('./lib/bst-bloomfilter');
 module.exports.AVLTree = require('./lib/avltree');
